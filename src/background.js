ff3e3a491fe358b2ce097ab8ec0f955a12f8f428
Joe Love <git@joelove.uk>
2019-01-04 15:55:25 +0000

    wire up content script

--- src/background.js
+++ src/background.js
@@ -5,37 +5,47 @@ import {
   construct,
   invoker,
   when,
-  identity,
+  head,
+  complement,
 } from 'ramda';
+
+import {
+  isNull,
+  isUndefined,
+} from 'lodash/fp';
 
 /* Constants */
 const INTRO_LABEL = 'Skip Intro';
-const RECAP_LABEL = 'Skip Recap';
+// const RECAP_LABEL = 'Skip Recap';
 
 /* Selectors */
 const SKIP_INTRO_SELECTOR = `[aria-label="${INTRO_LABEL}"]`;
 
 /* Utilities */
-const log = when(console.log, identity);
+const invoke = invoker(0);
 const invokeUnary = invoker(1);
+const isNotNull = complement(isNull);
+const isNotUndefined = complement(isUndefined);
 
 /* Getters */
 const addedNodesGetter = prop('addedNodes');
 
 /* Invokers */
+const invokeClick = invoke('click');
 const selectNode = invokeUnary('querySelector');
 
 /* Node Selectors */
-const selectSkipIntroAnchor = selectNode(SKIP_INTRO_SELECTOR);
 
 /* Constructors */
 const mutationObserverConstructor = construct(window.MutationObserver);
 
 /* Event Handlers */
-const onNodeAdded = pipe(selectSkipIntroAnchor, log);
-const onMutationObserved = pipe(addedNodesGetter, onNodeAdded);
+const clickNode = when(isNotNull, invokeClick);
+const selectSkipIntroAnchor = selectNode(SKIP_INTRO_SELECTOR);
+const clickSkipIntroAnchor = pipe(selectSkipIntroAnchor, clickNode);
+const onNodeAdded = when(isNotUndefined, clickSkipIntroAnchor);
+const onMutationObserved = pipe(addedNodesGetter, head, onNodeAdded);
 const onMutationsObserved = forEach(onMutationObserved);
-
 
 const mutationObserver = mutationObserverConstructor(onMutationsObserved);
 mutationObserver.observe(document.body, { childList: true, subtree: true });
