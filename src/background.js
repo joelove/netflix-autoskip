c41bcb8315b128deb44198d587e0501689da086b
Joe Love <git@joelove.dev>
2025-03-13 11:39:43 +0000

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,28 +1,21 @@
-import {
-  compose,
-  forEach,
-  head,
-  pipe,
-  when,
-} from 'ramda';
-
-import { SKIP_INTRO_SELECTOR } from './constants/element-selectors';
-
+import { compose, forEach, head, pipe, when } from 'ramda';
 import { addedNodesGetter } from './utilities/getters';
 import { mutationObserverConstructor } from './utilities/constructors';
 import { isNotNull, isElement } from './utilities/conditions';
 import { invoke, invokeUnary, invokeBinary } from './utilities/operations';
+import { SKIP_INTRO_SELECTOR } from './constants/element-selectors';
 
-const invokeClick = invoke('click');
-const selectNode = invokeUnary('querySelector');
 const observeNode = invokeBinary('observe');
-
 const observeDocument = observeNode(document.body, { childList: true, subtree: true });
 const documentMutationObserver = compose(observeDocument, mutationObserverConstructor, forEach);
 
+const selectNode = invokeUnary('querySelector');
 const selectSkipIntroAnchor = selectNode(SKIP_INTRO_SELECTOR);
+const invokeClick = invoke('click');
+
 const clickNode = when(isNotNull, invokeClick);
 const clickSkipIntroAnchor = pipe(selectSkipIntroAnchor, clickNode);
+
 const onNodeAdded = when(isElement, clickSkipIntroAnchor);
 const onMutationObserved = pipe(addedNodesGetter, head, onNodeAdded);
 
